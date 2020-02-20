6cde510310a7e6fd1daf6adb5faf2b07833cbb80
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-19 18:24:11 -0800

    forgot password added

--- wiki/forms.py
+++ wiki/forms.py
@@ -5,3 +5,8 @@ from wiki.models import Page
 class PageForm(forms.ModelForm):
     """ Render and process a form based on the Page model. """
     model = Page
+
+
+class FriendlyForm(forms.Form):
+    first_name = forms.CharField(max_length=100)
+    last_name = forms.CharField(max_length=100)
