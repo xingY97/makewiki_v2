e4078d92e45fd24aaaa7fda0753e1a36586dd7eb
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-19 18:55:53 -0800

    added accoutns view, but not working

--- makewiki/urls.py
+++ makewiki/urls.py
@@ -1,5 +1,4 @@
 """makewiki URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
@@ -28,6 +27,11 @@ urlpatterns = [
 
     # Wiki App
     path('', include('wiki.urls')),
-
+    
+    # Authentication
     path('accounts/', include('django.contrib.auth.urls')),
+    
+    # Signup
+    path('accounts/', include ('accounts.urls')),
+
 ]
